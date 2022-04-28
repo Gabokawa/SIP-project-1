d550c53e433bdad4509b447f1c79f3b2ca49d198
lmao-yeah <104477390+lmao-yeah@users.noreply.github.com>
2022-04-28 15:05:26 +0800

    js css html updates

--- main.js
+++ main.js
@@ -7,25 +7,31 @@
     if ((r == 255 && g < 255) && b == 0){
       g++;
       document.getElementById("lmao").style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+      document.getElementById("Dan").style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
     }
     else if (g == 255 && r > 0){
       r--;
       document.getElementById("lmao").style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+      document.getElementById("Dan").style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
     }
     else if (g == 255 && b < 255){
       b++;
       document.getElementById("lmao").style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+      document.getElementById("Dan").style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
     }
     else if (b == 255 && g > 0){
       g--;
       document.getElementById("lmao").style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+      document.getElementById("Dan").style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
     }
     else if (b == 255 && r < 255){
       r++;
       document.getElementById("lmao").style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+      document.getElementById("Dan").style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
     }
     else if (r == 255 && b > 0){
       b--;
       document.getElementById("lmao").style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+      document.getElementById("Dan").style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
     }
   }, 10);
